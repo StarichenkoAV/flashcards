707e24ecdf93ad9c0132f3e7759bd2601de4bf95
Anastasiia Shamrai <sky.blush@ya.ru>
2022-11-25 11:54:08 +0300

    update Topics component

--- views/Topics.jsx
+++ views/Topics.jsx
@@ -4,11 +4,15 @@ const Layout = require('./Layout');
 function Topics({ topics }) {
   return (
     <Layout>
-      <div className="container">
-        <h1>Topics:</h1>
+      <h1>Викторина</h1>
+      <h2>Выберите тему</h2>
+      <div className="cards-container">
         {topics.map((topic) => (
-          <div id={topic.id}>
-            {topic.title}
+          <div id={topic.id} className="card text-center" style={{ width: '25rem' }}>
+            <div className="card-body">
+              <h5 className="card-title">{topic.title}</h5>
+              <a href={`/cards/${topic.id}`} className="btn btn-info">Выбрать</a>
+            </div>
           </div>
         ))}
       </div>
