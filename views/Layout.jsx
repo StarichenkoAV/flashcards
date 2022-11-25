2a444de050dcf6407a7a2e467c1559d896365cc0
Anastasiia Shamrai <sky.blush@ya.ru>
2022-11-25 11:53:40 +0300

    add styles

--- views/Layout.jsx
+++ views/Layout.jsx
@@ -1,17 +1,25 @@
-const React = require('react')
+const React = require('react');
 
-const Layout = ({ children }) => {
-	return (
-		<html lang="en">
-			<head>
-				<meta charSet="UTF-8" />
-				<title>Title</title>
-			</head>
-			<body>
-				{children}
-			</body>
-		</html>
-	);
+function Layout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <title>Title</title>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+          crossOrigin="anonymous"
+        />
+        <link rel="stylesheet" type="text/css" href="/css/style.css" />
+      </head>
+
+      <body>
+        {children}
+      </body>
+    </html>
+  );
 }
 
-module.exports = Layout
+module.exports = Layout;
